32147589d4b4caf6832550b24f02f846dc23dd9d
flxxyz <flxxyz@users.noreply.github.com>
2020-05-12 08:02:35 +0000

    CI: build

---
+++ precache-manifest.e43ee0a71058547a0592bcded1b4d7e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896b37f15252589003ef65f432745d12",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de1dcefdd63061599916",
+    "url": "./static/css/2.b92e9812.chunk.css"
+  },
+  {
+    "revision": "b7f93f775ee75bf7d3cc",
+    "url": "./static/css/main.e157c700.chunk.css"
+  },
+  {
+    "revision": "de1dcefdd63061599916",
+    "url": "./static/js/2.b807c275.chunk.js"
+  },
+  {
+    "revision": "0fb9cc968fd20809e355d02e39686aa6",
+    "url": "./static/js/2.b807c275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f93f775ee75bf7d3cc",
+    "url": "./static/js/main.ff2f3470.chunk.js"
+  },
+  {
+    "revision": "cf748a83a70efe2f3614",
+    "url": "./static/js/runtime-main.b28e0b11.js"
+  }
+]);
